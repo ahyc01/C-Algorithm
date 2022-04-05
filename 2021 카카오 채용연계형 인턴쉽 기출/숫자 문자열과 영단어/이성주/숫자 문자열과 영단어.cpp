5847b198303b677862dd892abaf1231535e9f508
Sjlee <70562388+sjlee94@users.noreply.github.com>
2022-04-05 21:41:15 +0900

    Update 숫자 문자열과 영단어.cpp

--- 2021 카카오 채용연계형 인턴쉽 기출/숫자 문자열과 영단어/이성주/숫자 문자열과 영단어.cpp
+++ 2021 카카오 채용연계형 인턴쉽 기출/숫자 문자열과 영단어/이성주/숫자 문자열과 영단어.cpp
@@ -10,8 +10,9 @@ int solution(string s) {
                              "five", "six", "seven", "eight", "nine"};
     
     for(int i=0; i<number.size(); i++){
+        // 첫 번째 인자 : 반환할 대상, 두 번째 인자 : 정규표현식 개체(내가 찾고자 하는 문자), 세 번째 인자 : 바꿔줄 패턴
         answer = regex_replace(answer, regex(number[i]), to_string(i));
-    } 
+    }
     
     return stoi(answer);
 }
