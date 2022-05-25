1042800955390a635e659f7228a75ac1f7194e09
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-05-25 16:24:00 +0900

    Add files via upload

---
+++ 모임/2022.05.26/이친수/최아현/[백준]2193번-이친수.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{	
+	int N;
+	cin >> N;
+	
+	vector<long long> dp;
+
+	dp.push_back(1);
+	dp.push_back(1);
+
+	for (int i = 2; i < N; i++) {
+		dp.push_back(dp[i-1]+dp[i-2]);
+	}
+	cout << dp[N - 1];
+
+	return 0;
+}
+
