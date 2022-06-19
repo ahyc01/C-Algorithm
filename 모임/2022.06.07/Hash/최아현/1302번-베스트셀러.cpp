09e961308c2b87a0f0a1f760be932fcb4ce62e4b
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-06-19 22:45:42 +0900

    Add files via upload

---
+++ 모임/2022.06.07/Hash/최아현/1302번-베스트셀러.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	map<string, int> m;
+	int n, Vmax=0;
+	string x, Kmax;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (m.count(x)) { // 키 있으면 +1
+			m[x] += 1;
+		}
+		else { // 없으면 추가
+			m[x] = 1;
+		}
+	}
+
+	for (auto j : m) { // 맵안의 원소들 value max 찾기
+		if (Vmax < j.second) {
+			Vmax = j.second;
+			Kmax = j.first;
+		}
+	}
+	cout << Kmax;
+
+	return 0;
+}
