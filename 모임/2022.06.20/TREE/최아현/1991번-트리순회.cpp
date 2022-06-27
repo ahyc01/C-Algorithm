c4188017ece5be4950d0f8cdf23cd4921219fbac
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-06-27 17:12:33 +0900

    Add files via upload

---
+++ 모임/2022.06.20/TREE/최아현/1991번-트리순회.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct Node {
+
+	char c;
+	Node *left = NULL;
+	Node *right = NULL;
+};
+
+void pre(Node *node) {
+	cout << node->c;
+
+	if (node->left != NULL) {
+		pre(node->left);
+	}
+
+	if (node->right != NULL) {
+		pre(node->right);
+	}
+}
+void in(Node *node) {
+	if (node->left != NULL) {
+		in(node->left);
+	}
+
+	cout << node->c;
+
+	if (node->right != NULL) {
+		in(node->right);
+	}
+}
+void post(Node *node) {
+	if (node->left != NULL) {
+		post(node->left);
+	}
+
+	if (node->right != NULL) {
+		post(node->right);
+	}
+
+	cout << node->c;
+}
+int main() {
+
+	int N;
+	char p, l, r;
+	vector<Node> node(26);
+	Node * root;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> p >> l >> r;
+
+		node[p - 'A'].c = p;
+
+		if (l != '.') {
+			node[p - 'A'].left = &node[l - 'A'];
+		}
+		if (r != '.') {
+			node[p - 'A'].right = &node[r - 'A'];
+		}
+	}
+	root = &node[0];
+
+	pre(root);
+	cout << "\n";
+	in(root);
+	cout << "\n";
+	post(root);
+
+	return 0;
+}
